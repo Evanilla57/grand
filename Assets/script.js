16a1fe9f4c21b8c698b9db50f9f280433ef85eaf
Evanilla57 <grigg005@cougars.csusm.edu>
2023-07-23 15:56:58 -0700

    reorganized html to better reflect challenge criteria

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,18 @@
 var startBtn = document.querySelector("#startQuiz");
-var qOne = document.querySelector("#question-one")
 
-// function quizContent() {
-//     (quizGame.innerHTML == "test");
-// };
+var qOne = document.querySelector("#question-one");
+
+// var testQuestions[];
+
+// var testAnswers[];
 
 var quizGame = document.querySelector('#challenge');
 
 function displayQuiz() {
     function showQOne() {
-        quizGame.textContent = qOne;
-        qOne.style.visibility='visible'
+        quizGame.textContent = "test";
     }
     showQOne();
 }
 
 startBtn.addEventListener("click", displayQuiz);
-
