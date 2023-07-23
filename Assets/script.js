4f935c97cca976b06dcd2594c080305dbf3f6d71
Evanilla57 <grigg005@cougars.csusm.edu>
2023-07-23 14:35:56 -0700

    added event listener to Start Quiz button

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,14 @@
-console.log("JavaScript is ready to run");
+var startBtn = document.querySelector("#startQuiz");
+
+// function quizContent() {
+//     (quizGame.innerHTML == "test");
+// };
+
+//var quizGame = document.querySelector('#challenge');
+
+function test() {
+    console.log("test")
+}
+
+startBtn.addEventListener("click", test);
 
