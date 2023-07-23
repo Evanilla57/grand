0429a74538275d8a8b4df091f2ab055be908f7b5
Evanilla57 <grigg005@cougars.csusm.edu>
2023-07-23 16:54:39 -0700

    added function to allow timer to count down

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@ var startBtn = document.querySelector("#start");
 var challengeEl = document.querySelector("#challenge");
 var answersEl = document.querySelector("#answers");
 var resultEl = document.querySelector("#result");
-var timerEl = document.querySelector("#timer")
+var timerEl = document.querySelector("#timer");
 
 var questions = [
     {
@@ -26,17 +26,23 @@ var questions = [
     correct: "3" 
 }];
 
-// var testQuestions[];
-
-// var testAnswers[];
-
-var quizGame = document.querySelector('#challenge');
+function countDown() {
+    var timer = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timer);
+        }
+    },
+    1000
+    );
+}
 
 function displayQuiz() {
     // function showQOne() {
     //     quizGame.textContent = "test";
     }
     // showQOne();
-}
+// }
 
 startBtn.addEventListener("click", displayQuiz);
